d36edb15601b1e1058c9eea29b0a3f2e0469667e
subho991 <subhajitpatwari7@gmail.com>
2026-09-05 23:39:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ PORT = os.environ.get("PORT", "8001")
 DB_URI = os.environ.get("DATABASE_URL", "")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluooo")
 #--------------------------------------------
-FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "90"))  # 0 means no expiry
+FSUB_LINK_EXPIRY = int(os.getenv("FSUB_LINK_EXPIRY", "120"))  # 0 means no expiry
 BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "http://t.me/CallOwner_Bot")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
